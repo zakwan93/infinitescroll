9cbf4a4bfb9ce42a29e0098d2aac35d3906496e8
zakwan93 <zakwan_bhaiyat@yahoo.com>
2021-06-22 10:45:01 -0400

    Implemented Infinite scroll functinality

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader')
+
+
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
-const count = 10;
-const apiKey = 'W_DxvRqd36eagxZhnshP0yLX2UrLVdrKN0n1B1hTbSo';
+const count = 30;
+const apiKey = config.MY_UNSPLASH_API_KEY
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
+
+function imageLoaded(){
+    imagesLoaded++;
+    if(imagesLoaded === totalImages){
+        ready = true;
+        loader.hidden = true;
+    }
+}
 
 //Helper function to set attributes on DOM Elements
 function setAttributes(element, attributes){
@@ -14,6 +27,8 @@ function setAttributes(element, attributes){
 }
 
 function displayPhotos(){
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
     photosArray.forEach((photo) => {
         const item = document.createElement('a');
         // item.setAttribute('href', photos.links.html);
@@ -31,6 +46,9 @@ function displayPhotos(){
             alt: photo.alt_description,
             title: photo.alt_description
         });
+        
+        img.addEventListener('load', imageLoaded);
+
         item.appendChild(img);
         imageContainer.appendChild(item);
     })
@@ -45,5 +63,12 @@ async function getPhotos(){
         // Catch Error Here
     }
 }
+
+window.addEventListener('scroll', () => {
+    if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready){
+        ready = false;
+        getPhotos();
+    }
+})
 
 getPhotos();
